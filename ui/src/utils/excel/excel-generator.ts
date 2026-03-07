2b6d1740eba81b899ff51d3d039e38dcafc71be7
iHoshiii <aldwinloreto690@gmail.com>
2026-03-07 14:03:36 +0800

    previously married fixed

--- ui/src/utils/excel/excel-generator.ts
+++ ui/src/utils/excel/excel-generator.ts
@@ -227,20 +227,20 @@ export class ExcelGenerator {
 
             // Previous Marriage Dissolved Logic (Groom)
             if (data.gStatus !== "Single") {
-                appSheet.getCell('B18').value = this.sanitize(data.gDissolvedHow).toUpperCase();
-                appSheet.getCell('B19').value = this.sanitize(data.gDissolvedPlace).toUpperCase();
-                appSheet.getCell('L19').value = this.sanitize(data.gDissolvedCountry).toUpperCase();
+                appSheet.getCell('B18').value = this.sanitize(data.gDissolvedHow);
+                appSheet.getCell('B19').value = this.sanitize(data.gDissolvedPlace);
+                appSheet.getCell('L19').value = this.sanitize(data.gDissolvedCountry);
                 appSheet.getCell('B20').value = this.formatBirthday(data.gDissolvedDate || "");
-                appSheet.getCell('B21').value = this.sanitize(data.gRelationshipDegree).toUpperCase();
+                appSheet.getCell('B21').value = this.sanitize(data.gRelationshipDegree);
             }
 
             // Previous Marriage Dissolved Logic (Bride)
             if (data.bStatus !== "Single") {
-                appSheet.getCell('U18').value = this.sanitize(data.bDissolvedHow).toUpperCase();
-                appSheet.getCell('B19').value = this.sanitize(data.bDissolvedPlace).toUpperCase(); // User said B19 for female too
-                appSheet.getCell('AE19').value = this.sanitize(data.bDissolvedCountry).toUpperCase();
+                appSheet.getCell('U18').value = this.sanitize(data.bDissolvedHow);
+                appSheet.getCell('B19').value = this.sanitize(data.bDissolvedPlace); // User said B19 for female too
+                appSheet.getCell('AE19').value = this.sanitize(data.bDissolvedCountry);
                 appSheet.getCell('U20').value = this.formatBirthday(data.bDissolvedDate || "");
-                appSheet.getCell('U21').value = this.sanitize(data.bRelationshipDegree).toUpperCase();
+                appSheet.getCell('U21').value = this.sanitize(data.bRelationshipDegree);
             }
 
             // Registry Number Logic
